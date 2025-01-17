4ddfaa8110d93d20cc2e397063cbe9d720aafb26
nishant talaviya <nishanttalaviya2308@gmail.com>
2025-01-17 22:27:09 +0530

    aa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,9 @@
-import Adminapp from "./adminpanel/Components/Adminapp";
-import Booking from "./userpanel/Components/Booking ";
-import Confirm from "./userpanel/Components/Confirm";
-import Forget from "./userpanel/Components/forget";
-import Maintenance from "./userpanel/Components/Maintenance ";
-import Reset from "./userpanel/Components/Reset";
-import Signin from "./userpanel/Components/Signin";
-import SignIn from "./userpanel/Components/Signin";
+import Userapp from "./userpanel/Components/Userapp";
 
 function App() {
   return (
     <>
-        <Booking/>
+        <Userapp/>
 
       </>
     
